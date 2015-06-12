939c73a45207448f965b3399d7374f869424767e
evalidate <deievalidate@gmail.com>
2015-06-12 08:32:02 +0530

    Update apache_config.sh

--- scripts/apache_config.sh
+++ scripts/apache_config.sh
@@ -6,6 +6,6 @@ apache_vhost_file="/etc/apache2/sites-available/default"
 # Add proxy pass lines at the end of the apache config file
 # using sed
 sed -i '/<\/VirtualHost>/i \
-ProxyPassMatch    ^/web(.*) http://localhost:8080/evalidate-dei-1.0-SNAPSHOT/$1\
-ProxyPassReverse  ^/web(.*) http://localhost:8080/evalidate-dei-1.0-SNAPSHOT/$1
+ProxyPassMatch    / http://localhost:8080/evalidate-dei-1.0-SNAPSHOT/
+ProxyPassReverse  / http://localhost:8080/evalidate-dei-1.0-SNAPSHOT/
 ' $apache_vhost_file
